9a1be0e244cffe3935790761d5e7957ba4b79e57
fajar-git-cloud <muhfaj06@gmail.com>
2025-07-27 21:43:07 +0700

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+ output: "export", 
+  basePath: "/smartwatch-landing-page", 
+  assetPrefix: "/smartwatch-landing-page/",
 };
 
 export default nextConfig;
